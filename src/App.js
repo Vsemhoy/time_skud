cb69a68e1b3588879efe66ae22888315374929d8
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2025-03-24 00:44:28 +0300

    UIp

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 import HomePage from './modules/DEFAULT_PAGE/HomePage';
 import UserListPage from './modules/USER_LIST/UserListPage';
 import UserPage from './modules/USER_PAGE/UserPage';
-import { Breadcrumb, Layout, Menu, Skeleton, theme } from 'antd';
+import { Breadcrumb, Layout, Menu, Skeleton, theme, Input, Dropdown, Avatar } from 'antd';
 import MenuItem from 'antd/es/menu/MenuItem';
 import { useEffect, useState } from 'react';
 import { DS_USER } from './CONFIG/DEFAULTSTATE';
@@ -23,6 +23,8 @@ import ProdCalManagerPage from './modules/PROD_CAL_MANAGER/ProdCalManagerPage';
 import GroupManagerPage from './modules/GROUP_MANAGER/GroupManagerPage';
 import EventRandomixer from './modules/EVENT_RANDOMIXER/EventRandomixer';
 import AccountPage from './modules/ACCOUNT/AccountPage';
+import Search from 'antd/es/input/Search';
+import { HomeOutlined, LogoutOutlined, SettingOutlined, SmileOutlined, ThunderboltOutlined, UserOutlined } from '@ant-design/icons';
 
 
 const { Header, Content, Footer } = Layout;
@@ -113,21 +115,39 @@ function App() {
     }, []);
 
 
+    const userMenu = (
+      <Menu>
+        <Menu.Item key="status">Статус: Онлайн</Menu.Item>
+        <Menu.Item key="settings" icon={<SettingOutlined />}>
+          Настройки
+        </Menu.Item>
+        <Menu.Item key="block" icon={<ThunderboltOutlined />}>
+          Заблокировать
+        </Menu.Item>
+        <Menu.Item key="logout" icon={<LogoutOutlined />}>
+          Выйти
+        </Menu.Item>
+      </Menu>
+    );
+    
+
   return (
     <Layout style={{background: '#fff'}}>
           <Router>
           <div >
-      <Header style={{ display: 'flex', alignItems: 'center', background: 'white' }}
+      {/* <Header style={{ display: 'flex', alignItems: 'center', background: '#001529' }}
       className={'sk-main-menu'}
       >
         <div className="demo-logo" />
         <Menu
-          theme="light"
+          theme="dark"
+          style={{ flex: 1, minWidth: 0 }}
           mode="horizontal"
           defaultSelectedKeys={['2']}
-          style={{ flex: 1, minWidth: 0 }}
         >
-
+            <div>
+              <Search></Search>
+            </div>
             <MenuItem
               key={'menu_5234'}>
               <a href={HTTP_ROOT}>Home</a>
@@ -138,6 +158,7 @@ function App() {
               <Link
                 onClick={()=>{ setLocation('me')}}
               >Моё</Link>
+              
             </MenuItem>
 
             <MenuItem
@@ -177,6 +198,90 @@ function App() {
               >Заявки на смену графика</Link>
             </MenuItem> */}
 
+            {/* <MenuItem
+            key={'menu_5234765'}>
+              <Link 
+              onClick={()=>{ setLocation('admin/groups')}}
+              >Группы пользоваателей</Link>
+            </MenuItem>
+
+            <MenuItem
+            key={'menu_5235644'}>
+              <Link 
+              onClick={()=>{ setLocation('superadmin/randomixer')}}
+              >RMXR</Link>
+            </MenuItem> */}
+
+
+            {/* <MenuItem>
+              <Link 
+              onClick={()=>{ setLocation('admin')}}
+              >Skud Admin</Link>
+            </MenuItem> */}
+
+            
+            {/* <MenuItem>
+              <Link 
+              onClick={()=>{ setLocation('admincalendar')}}
+              >Произв. календари</Link>
+            </MenuItem> 
+          </Menu>
+      </Header> */}
+
+      <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+        className={'sk-main-menu'}
+      >
+        {/* Первая группа */}
+        <Menu mode="horizontal" style={{ background: '#00000000', flex: 1}}>
+          <Menu.Item key="home" icon={<HomeOutlined  style={{ fontSize: '20px', color: '#fff', marginTop: '6px',
+           textAlign: 'center', paddingLeft: '6px' }} />} ><a href={HTTP_ROOT}></a></Menu.Item>
+          <Menu.SubMenu key="menu1" title="Персональный раздел">
+            <MenuItem
+            key={'menu_52d34'}>
+              <Link
+                onClick={()=>{ setLocation('me')}}
+              >Моё</Link>
+              
+            </MenuItem>
+
+            <MenuItem
+            key={'menu_52s34'}>
+              <Link 
+                onClick={()=>{ setLocation('userlist')}}
+              >Сотрудники</Link>
+            </MenuItem>
+          </Menu.SubMenu>
+          <Menu.SubMenu key="menu2" title="Управление сотрудниками">
+          <MenuItem
+            key={'menu_52342'}>
+              <Link 
+              onClick={()=>{ setLocation('admin/rules')}}
+              >Правила</Link>
+            </MenuItem>
+
+            <MenuItem
+            key={'menu_5234qq'}>
+              <Link 
+              onClick={()=>{ setLocation('admin/schedules')}}
+              >Графики</Link>
+            </MenuItem>
+
+
+
+            <MenuItem
+            key={'menu_235234'}>
+              <Link 
+              onClick={()=>{ setLocation('admin/prodcals')}}
+              >Календари</Link>
+            </MenuItem>
+
+
+            <MenuItem>
+              <Link 
+              onClick={()=>{ setLocation('admin/schedules')}}
+              >Заявки на смену графика</Link>
+            </MenuItem> 
+
             <MenuItem
             key={'menu_5234765'}>
               <Link 
@@ -190,30 +295,33 @@ function App() {
               onClick={()=>{ setLocation('superadmin/randomixer')}}
               >RMXR</Link>
             </MenuItem>
+          </Menu.SubMenu>
+        </Menu>
 
+        {/* Вторая группа */}
+        <Input.Search placeholder="Поиск" style={{ maxWidth: '300px', margin: '0 20px' }} />
 
+        {/* Третья группа */}
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Dropdown overlay={userMenu} trigger={['click']}>
+            <div style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+              <Avatar style={{ backgroundColor: '#87d068', marginRight: '8px' }}>
+                { userAct ? (
+                  <SmileOutlined  style={{ fontSize: '36px', color: '#08c' }} />
+                ) : (
+                  <ThunderboltOutlined />
+                )}
+              </Avatar>
+              { userAct ? (
+                <span>{userAct.user.name} {userAct.user.patronymic}</span>
+              ) : (
+              <span>Пользователь</span>
 
-
-            {/* <MenuItem>
-              <Link 
-              onClick={()=>{ setLocation('admin')}}
-              >Skud Admin</Link>
-            </MenuItem> */}
-
-
-
-
-
-
-            
-            {/* <MenuItem>
-              <Link 
-              onClick={()=>{ setLocation('admincalendar')}}
-              >Произв. календари</Link>
-            </MenuItem> */}
-          </Menu>
+              )}
+            </div>
+          </Dropdown>
+        </div>
       </Header>
-
 
       <Content style={{ padding: '0 48px' }}>
         
