51382bac8bcb5ac50c9cd988f5a107938652f40b
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2025-05-26 02:15:09 +0300

    up

--- src/modules/ADMIN/ACLSKUD/AclSkudPage2.jsx
+++ src/modules/ADMIN/ACLSKUD/AclSkudPage2.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, uuid } from 'react';
 import './components/style/aclskud2.css';
-import { BuildOutlined, CheckCircleOutlined, ClearOutlined, CloseCircleOutlined, CopyOutlined, DeleteOutlined, DiffOutlined, DownSquareOutlined, EditOutlined, EyeOutlined, UserSwitchOutlined } from '@ant-design/icons';
+import { BarsOutlined, BuildOutlined, CheckCircleOutlined, CheckOutlined, ClearOutlined, CloseCircleOutlined, ClusterOutlined, CopyOutlined, DeleteColumnOutlined, DeleteOutlined, DiffOutlined, DownSquareOutlined, EditOutlined, EyeOutlined, UserSwitchOutlined } from '@ant-design/icons';
 import { AimOutlined, BlockOutlined, BugOutlined, CarOutlined, DockerOutlined, DollarOutlined, MedicineBoxOutlined, MoonOutlined, PlusCircleOutlined, PlusOutlined, RocketOutlined, SmileOutlined, TruckOutlined } from "@ant-design/icons";
-import { Affix, Checkbox, Radio, Select, Tabs } from 'antd';
+import { Affix, Checkbox, Dropdown, Radio, Select, Tabs } from 'antd';
 import { CSRF_TOKEN, PRODMODE } from '../../../CONFIG/config';
 import { DS_DEPARTMENTS, DS_USERLIST_USERS } from '../../../CONFIG/DEFAULTSTATE';
 import AclSkudCardRow from './components/AclSkudCardRow';
@@ -36,7 +36,60 @@ const AclSkudPage2 = (props) => {
     const [companiesOptions, setCompaniesOptions] = useState([]);
     const [openedTemplates, setOpenedTemplates] = useState([]);
     const [openedDeparts, setOpenedDeparts] = useState([]);
+    const [openedUsers, setOpenedUsers] = useState([]);
     
+    const rowMenuItems = [
+      {
+        key: '1',
+        label: (
+          <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+            Скопировать доступы
+          </a>
+        ),
+      },
+      {
+        key: '2',
+        label: (
+          <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+            Вставить доступы
+          </a>
+        ),
+      },
+      {
+        key: '3',
+        label: (
+          <div>
+            Очистить доступы
+          </div>
+        ),
+      },
+      {
+        key: '4',
+        label: (
+          <div>
+            Установить всем в отделе
+          </div>
+        ),
+      },
+      {
+        key: '5',
+        label: (
+          <div>
+            Установить в шаблон
+          </div>
+        ),
+      },
+      {
+        key: '4',
+        label: (
+          <div>
+            Установить всем выделенным
+          </div>
+        ),
+      },
+    ];
+
+
     useEffect(() => {
       if (PRODMODE){
         //   get_departments();
@@ -116,8 +169,49 @@ const toggleTemplatesOpen = (ev, id) => {
 const toggleDepartsOpen = (ev, id) => {
   console.log('ev', ev)
   if (ev.shiftKey){
+    let opened = openedDeparts.includes(id);
+    if (opened){
+      setOpenedDeparts([]);
+    } else {
+      let ndeps = departments.map(itm => itm.id);
+      setOpenedDeparts(ndeps);
+    }
   } else {
     setOpenedDeparts(prev => {
+      if (prev.includes(id)) {
+        // Если id есть — удаляем его
+        return prev.filter(item => item !== id);
+      } else {
+        // Если нет — добавляем
+        return [...prev, id];
+      }
+    });
+  }
+};
+
+const toggleUsersOpen = (ev, id, dep_id) => {
+  if (ev.shiftKey){
+    // if user opened - close all / else open all
+    let opened = openedUsers.includes(id);
+    let usersToOperate = userCollection.filter((item) => item.depart_id == dep_id).map(itm => itm.id);
+    let nuds = [];
+    if (opened){
+      for (let index = 0; index < openedUsers.length; index++) {
+        if (!usersToOperate.includes(openedUsers[index])){
+          nuds.push(openedUsers[index]);
+        }
+      }
+    } else {
+      nuds = JSON.parse(JSON.stringify(openedUsers));
+      for (let index = 0; index < usersToOperate.length; index++) {
+        if (!nuds.includes(usersToOperate[index])){
+          nuds.push(usersToOperate[index]);
+        }
+      }
+    }
+    setOpenedUsers(nuds);
+  } else {
+    setOpenedUsers(prev => {
       if (prev.includes(id)) {
         // Если id есть — удаляем его
         return prev.filter(item => item !== id);
@@ -133,6 +227,19 @@ const toggleDepartsOpen = (ev, id) => {
   const toggleDoubleClickDepart = (row_id) => {
     toggleDepartsOpen({}, row_id);
   }
+
+
+  const setColumnChecked = (ev, targtype, column, id ) =>
+  {
+    console.log(targtype, column, id );
+  }
+  
+  const setRowChecked = (ev, targtype, column, id ) =>
+  {
+      
+      console.log(targtype, column, id );
+  }
+
 
     return (
         <div className='sk-page-body'>
@@ -178,6 +285,7 @@ const toggleDepartsOpen = (ev, id) => {
                     <div className={'sk-table-aclskud'}>
                         <Affix offsetTop={0}>                
                             <div className={'sk-table-aclskud-row sk-table-aclskud-header'}>
+                            <div className='sk-tas-inwrap'>
                                 <div>
                                     {/* <Checkbox></Checkbox> */}
                                 </div>
@@ -197,15 +305,6 @@ const toggleDepartsOpen = (ev, id) => {
                                     gridTemplateColumns: `repeat(${aclColumns.length}, 1fr)`,
                                   }}
                                 >
-                                    {/* <div title='Создание '>Созд.<DiffOutlined /></div>
-                                    <div title='Редактирование'>Ред.<EditOutlined /></div>
-                                    <div title='Согласование и отклонение'>Согл.<CheckCircleOutlined /></div>
-                                    <div title='Создание'>Созд.<DiffOutlined /></div>
-                                    <div title='Редактирование'>Ред.<EditOutlined /></div>
-                                    <div title='Согласование и отклонение'>Согл.<CheckCircleOutlined /></div>
-                                    <div title='Создание'>Созд.<DiffOutlined /></div>
-                                    <div title='Редактирование'>Ред.<EditOutlined /></div>
-                                    <div title='Согласование и отклонение'>Согл.<CheckCircleOutlined /></div> */}
                                     {aclColumns.map((item)=>(
                                       <div 
                                       className='sk-table-aclskud-cell'
@@ -224,21 +323,22 @@ const toggleDepartsOpen = (ev, id) => {
                             </div>
         
                             <div>
-        
+                            </div>
                             </div>
                         </div>
                         </Affix>
 
                         <div>
                           {departments.map(item => (
-                            <div>
+                            <div className={`${openedDeparts.includes(item.id) || openedTemplates.includes(item.id) ? "sk-opened-box":""}`}>
                             <div 
                               onDoubleClick={()=>{toggleDoubleClickDepart(item.id)}}
                               className={`sk-table-aclskud-row sk-table-aclskud-data `} key={`checkdser${item.id}_${item.id}`}>
+                              <div className='sk-tas-inwrap'>
                               <div><Checkbox /></div>
                               <div><div>{item.id}</div></div>
                               <div className={'sk-table-aclskud-row-name'}><div className={'sk-flex-space'}>
-                                <span>{item.name}</span>
+                                <span style={{ fontWeight: '500', color: '#6189aa'}}>{item.name}</span>
                                 </div></div>
                               <div>
                                 <div className={'sk-table-aclskud-multicol2'}
@@ -252,7 +352,7 @@ const toggleDepartsOpen = (ev, id) => {
                                         style={{background: `${item.color}26`}}
                                         title={item.text}
                                       >
-                                        <div><div>.</div></div>
+                                        <div><div></div></div>
                                       
                                       </div>
                                     ))}
@@ -269,6 +369,7 @@ const toggleDepartsOpen = (ev, id) => {
                                 ><BuildOutlined /></div>
                                 {/* <div title='Очистить строку' ><ClearOutlined /></div> */}
                               </div>
+                              </div>
                             </div>
 
 
@@ -277,8 +378,8 @@ const toggleDepartsOpen = (ev, id) => {
                                   {eventTypes.map(type => (
                                     
                                 <div 
-                                  // onDoubleClick={()=>{toggleDoubleClickDepart(item.id)}}
                                   className={`sk-table-aclskud-row sk-table-aclskud-data `} key={`checktempl${item.id}_${type.id}`}>
+                                  <div className='sk-tas-inwrap'>
                                   <div><Checkbox /></div>
                                   <div><div>{type.id}</div></div>
                                   <div className={'sk-table-aclskud-row-name'}><div className={'sk-flex-space'}>
@@ -305,7 +406,7 @@ const toggleDepartsOpen = (ev, id) => {
                                     </div>
                                   </div>
                                   <div className='sk-flex sk-table-triggers'>
-                                    
+                                    </div>
                                 </div>
                                 </div>
                                   ))}
@@ -327,10 +428,114 @@ const toggleDepartsOpen = (ev, id) => {
                             
                             { openedDeparts.includes(item.id) && (
                               <div>
+                              <div>
                                   {userCollection.filter((user)=> user.depart_id === item.id).map(user => (
-                                    <div className={'sk-table-aclskud-row sk-table-aclskud-data sk-act-subrow'}>{user.name}</div>
+                                    <div 
+                                      onDoubleClick={(ev)=>{toggleUsersOpen(ev, user.id, item.id)}}
+                                  className={`sk-table-aclskud-row sk-table-aclskud-data `} key={`checkusr${item.id}_${user.id}`}>
+                                  <div className={`sk-tas-inwrap sk-tas-userrow ${openedUsers.includes(user.id) ? 'sk-tas-userrow-border' : ''}`}>
+                                  <div><Checkbox /></div>
+                                  <div><div>{user.id}</div></div>
+                                  <div className={'sk-table-aclskud-row-name'}><div className={'sk-flex-space'}>
+                                    <span>{user.surname} {user.name} {user.secondname}</span>
+                                    </div></div>
+                                  <div>
+                                    <div className={'sk-table-aclskud-multicol2'}
+                                    style={{
+                                        gridTemplateColumns: `repeat(${aclColumns.length}, 1fr)`,
+                                      }}
+                                    >
+                                    {aclColumns.map((item)=>(
+                                          <div 
+                                            className='sk-table-aclskud-cell'
+                                            style={{background: `${item.color}26`}}
+                                            title={item.text}
+                                          >
+                                            <div className='sk-false-check'
+                                            onMouseDown={(ev)=>{setColumnChecked(ev, 'user', item.id, user.id)}}
+                                            >
+                                            <div>
+                                              <CheckOutlined />
+                                             </div>
+                                             </div>
+                                          
+                                          </div>
+                                        ))}
+                                    </div>
+                                  </div>
+                                  <div className='sk-flex sk-table-triggers'>
+                                    <div title='Пользователи отдела'
+                                    className={`${openedUsers.includes(user.id) ? 'sk-trigger-active' : ''}`}
+                                    onMouseDown={(ev)=>{toggleUsersOpen(ev, user.id, item.id)}}
+                                  >
+                                    {openedUsers.includes(user.id) ? (
+                                      <DeleteColumnOutlined />
+                                    ) : (
+                                      <ClusterOutlined />
+                                    )}
+                                  </div>
+                                    <div>
+                                      <Dropdown menu={{items: rowMenuItems}}>
+                                        <BarsOutlined />
+                                      </Dropdown>
+                                    </div>
+                                </div>
+                                </div>
+                                 <div className={'sk-aclskud-row-inner'}>
+                                    {openedUsers.includes(user.id) && (
+                                      <div className='sk-act-depusererow'>
+                                        {eventTypes.map(type => (
+                                        <div 
+                                          className={`sk-table-aclskud-row sk-table-aclskud-data `} key={`checktempl${item.id}_${type.id}`}>
+                                          <div className='sk-tas-inwrap'>
+                                          <div></div>
+                                          <div><div>{type.id}</div></div>
+                                          <div className={'sk-table-aclskud-row-name'}
+                                            title={type.text}
+                                          ><div className={'sk-flex-space'}>
+                                            <span>{type.title}</span>
+                                            </div></div>
+                                          <div>
+                                            <div className={'sk-table-aclskud-multicol2'}
+                                            style={{
+                                                gridTemplateColumns: `repeat(${aclColumns.length}, 1fr)`,
+                                              }}
+                                            >
+                                            {aclColumns.map((item)=>(
+                                                  <div 
+                                                    className='sk-table-aclskud-cell'
+                                                    style={{background: `${item.color}26`}}
+                                                    title={item.text}
+                                                  >
+                                                    <div><div><AclCheckbox
+
+                                                    /></div></div>
+                                                  
+                                                  </div>
+                                                ))}
+                                            </div>
+                                          </div>
+                                          <div className='sk-flex sk-table-triggers'>
+                                            <div className='sk-false-check'
+                                              onMouseDown={(ev)=>{setRowChecked(ev, 'user', type.id, user.id)}}
+                                            >
+                                              <div>
+                                                <CheckOutlined />
+                                              </div>
+                                            </div>
+                                            <div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                        </div>
+                                          ))}
+                                      </div>
+                                    )}
+                                  </div>
+                                 </div>
                                   ))
                                   }
+                              </div>
                               </div>
                               )}
                             </div>
