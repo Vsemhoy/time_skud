eeec61ac6d6d6590577f0572bd4ec78de6217a1a
TefteleCom <vsemhoy@live.ru>
2025-07-03 17:43:11 +0300

    up

--- src/CONFIG/config.js
+++ src/CONFIG/config.js
@@ -3,7 +3,7 @@ export const PRODMODE = (!(window.location.hostname === 'localhost' || window.lo
 export const CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]') ? document.querySelector('meta[name="csrf-token"]').content : null;
 export const HTTP_HOST  = document.querySelector('meta[name="host"]') ? document.querySelector('meta[name="host"]').content : null;
 export const HTTP_ROOT = HTTP_HOST ? HTTP_HOST.replace('time', '') : '';
-export const HOST_COMPONENT_ROOT = !PRODMODE ? '' : '/com/timeskud';
+export const HOST_COMPONENT_ROOT = !PRODMODE ? '' : '/com/skud';
 
 export const BASE_NAME = PRODMODE ? '/skud' : '/';
 export const BASE_ROUTE = PRODMODE ? '/skud' : '';
