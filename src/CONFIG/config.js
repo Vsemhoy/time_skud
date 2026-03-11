d90b3671a1dfd8ad2c5be891a8bc9e728ef36f43
Andrew Sharanda <azzimandias@gmail.com>
2026-03-11 18:08:59 +0300

    update

--- src/CONFIG/config.js
+++ src/CONFIG/config.js
@@ -1,12 +1,19 @@
 export const PRODMODE = (!(window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'));
 // Тянем токен со страницы
 export const CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]') ? document.querySelector('meta[name="csrf-token"]').content : null;
-export const HTTP_HOST  = document.querySelector('meta[name="host"]') ? document.querySelector('meta[name="host"]').content : null;
-export const HTTP_ROOT = HTTP_HOST ? HTTP_HOST.replace('time', '') : '';
+/*export const HTTP_HOST  = document.querySelector('meta[name="host"]') ? document.querySelector('meta[name="host"]').content : null;
+export const HTTP_ROOT = HTTP_HOST ? HTTP_HOST.replace('time', '') : '';*/
+export const HTTP_HOST = window.location.hostname; // только IP или домен, без порта
+export const HTTP_ROOT = `http://${window.location.hostname}`;
+export const FRONT_PORT = window.location.port;
+export const HTTP_ROOT_FRONT = `http://${window.location.hostname}:${FRONT_PORT}`;
 export const HOST_COMPONENT_ROOT = !PRODMODE ? '' : '/com/skud';
 
-export const BASE_NAME = PRODMODE ? '/skud' : '/';
-export const BASE_ROUTE = PRODMODE ? '/skud' : '';
+/*export const BASE_NAME = PRODMODE ? '/skud' : '/';
+export const BASE_ROUTE = PRODMODE ? '/skud' : '';*/
+
+export const BASE_NAME = PRODMODE ? '/' : '/';
+export const BASE_ROUTE = PRODMODE ? '' : '';
 
 // Websocket backend-for-frontend PORT
 export const BFF_PORT = 5003;
