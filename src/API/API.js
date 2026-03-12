71607561d5fb8b2f953681c26cc9101711b7d52f
Andrew Sharanda <azzimandias@gmail.com>
2026-03-12 12:42:23 +0300

    update

--- src/API/API.js
+++ src/API/API.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-import {HTTP_ROOT} from "../CONFIG/config";
+import {HTTP_ROOT, HTTP_HOST} from "../CONFIG/config";
 
 //const PROD_API_URL = 'http://89.104.68.50'
 // export const PROD_API_URL = 'https://pulse-retail.ru'
+axios.defaults.withCredentials = true;
 export const PROD_AXIOS_INSTANCE = axios.create({
     baseURL: HTTP_ROOT,
     timeout: 10000,
